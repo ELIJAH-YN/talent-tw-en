2b8b22a3f1d290ff660b8ca2add430622ce2e484
Chill Wiing <chillwiing@Chills-MacBook-Air.local>
2019-08-01 00:23:32 +0800

    finial v2

--- resources/views/official-en/layout/footer.blade.php
+++ resources/views/official-en/layout/footer.blade.php
@@ -42,6 +42,16 @@
                     <h5 class="host_position">The Sponsor</h5>
                 </div>
             </div>
+            <div class="main_host">
+                <div class="main_host">
+                    <div class="company_container">
+                        <div class="company_logo">
+                            <img src="/images/sponsor/kamisama-logo.jpg" class="company_logo">
+                        </div>
+                        <h5 class="host_position">Technical supply</h5>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
