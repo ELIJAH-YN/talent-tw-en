a98911a38ef7a1ea3bd5108ad36844c208015265
Chill Wiing <chillwiing@Chills-MacBook-Air.local>
2019-08-20 15:12:17 +0800

    fix vn th footer company name and sponsor

--- resources/views/official-vn/layout/footer.blade.php
+++ resources/views/official-vn/layout/footer.blade.php
@@ -18,39 +18,39 @@
                     <h5 class="host_position">Đơn vị tổ chức</h5>
                 </div>
             </div>
-            <div class="co_host">
-                <div class="company_container">
-                    <div class="company_logo">
-                        <img src="/images/sponsor/sponsor-06.jpg" alt="" class="the-co-organis">
-                        <img src="/images/sponsor/sponsor-15.jpg" alt="" class="the-co-organis">
-                        <img src="/images/sponsor/sponsor-21.jpg" alt="" class="the-co-organis">
-                        <img src="/images/sponsor/sponsor-01.jpg" alt="" class="the-co-organis">
-                        <img src="/images/sponsor/sponsor-17.jpg" alt="" class="the-co-organis">
-                    </div>
-                    <h5 class="host_position">Đơn vị hỗ trợ thực hiện</h5>
-                </div>
-            </div>
-            <div class="co_host">
-                <div class="company_container">
-                    <div class="company_logo">
-                        <img src="/images/sponsor/sponsor-18.jpg" class="the-co-organis">
-                        <img src="/images/sponsor/sponsor-22.png" class="the-co-organis">
-                        <img src="/images/sponsor/sponsor-14.jpg" class="the-co-organis">
-                        <img src="/images/sponsor/sponsor-02.jpg" class="the-co-organis">
-                        <img src="/images/sponsor/sponsor-03.jpg" class="the-co-organis">
-                        <img src="/images/sponsor/sponsor-24.jpg" class="the-co-organis">
-                    </div>
-                    <h5 class="host_position">Đơn vị tài trợ</h5>
-                </div>
-            </div>
-            <div class="main_host">
-                <div class="company_container">
-                    <div class="company_logo">
-                        <img src="/images/sponsor/kamisama-logo.jpg" class="company_logo">
-                    </div>
-                    <h5 class="host_position">Cung cấp kỹ thuật</h5>
-                </div>
-            </div>
+{{--            <div class="co_host">--}}
+{{--                <div class="company_container">--}}
+{{--                    <div class="company_logo">--}}
+{{--                        <img src="/images/sponsor/sponsor-06.jpg" alt="" class="the-co-organis">--}}
+{{--                        <img src="/images/sponsor/sponsor-15.jpg" alt="" class="the-co-organis">--}}
+{{--                        <img src="/images/sponsor/sponsor-21.jpg" alt="" class="the-co-organis">--}}
+{{--                        <img src="/images/sponsor/sponsor-01.jpg" alt="" class="the-co-organis">--}}
+{{--                        <img src="/images/sponsor/sponsor-17.jpg" alt="" class="the-co-organis">--}}
+{{--                    </div>--}}
+{{--                    <h5 class="host_position">Đơn vị hỗ trợ thực hiện</h5>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--            <div class="co_host">--}}
+{{--                <div class="company_container">--}}
+{{--                    <div class="company_logo">--}}
+{{--                        <img src="/images/sponsor/sponsor-18.jpg" class="the-co-organis">--}}
+{{--                        <img src="/images/sponsor/sponsor-22.png" class="the-co-organis">--}}
+{{--                        <img src="/images/sponsor/sponsor-14.jpg" class="the-co-organis">--}}
+{{--                        <img src="/images/sponsor/sponsor-02.jpg" class="the-co-organis">--}}
+{{--                        <img src="/images/sponsor/sponsor-03.jpg" class="the-co-organis">--}}
+{{--                        <img src="/images/sponsor/sponsor-24.jpg" class="the-co-organis">--}}
+{{--                    </div>--}}
+{{--                    <h5 class="host_position">Đơn vị tài trợ</h5>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--            <div class="main_host">--}}
+{{--                <div class="company_container">--}}
+{{--                    <div class="company_logo">--}}
+{{--                        <img src="/images/sponsor/kamisama-logo.jpg" class="company_logo">--}}
+{{--                    </div>--}}
+{{--                    <h5 class="host_position">Cung cấp kỹ thuật</h5>--}}
+{{--                </div>--}}
+{{--            </div>--}}
         </div>
     </div>
 
@@ -58,10 +58,10 @@
         <div class="row">
             <div class="col-sm-12">
                 <p class="company-information">
-                    快拍股份有限公司<br>
-                    地址：台北市大安區和平東路二段118巷2弄24號<br>
-                    客服專線:02-2736-1119<br>
-                    傳真專線:02-2736-1109<br>
+                    Quick Pi Company Limited<br>
+                    Address：No. 24, Lane 2, Lane 118, Section 2, Heping East Road, Daan District, Taipei City<br>
+                    Customer Service Hotline:02-2736-1119<br>
+                    Fax line:02-2736-1109<br>
                 </p>
                 <div class="row">
                     <ul class="sci">
