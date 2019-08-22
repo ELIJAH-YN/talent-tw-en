5bf08566232bc0f6d5f5c802e9ae8aadeb1e874f
tales <talesworks@gmail.com>
2019-08-22 16:02:58 +0800

    新增語系檔

--- public/assets/lang_dict/register_form/zh.js
+++ public/assets/lang_dict/register_form/zh.js
@@ -1,3 +1,58 @@
 const dict = {
-    msg: 'zh msg'
+    
+    require: '必填',
+    form_field: {
+        name: {
+            placeholder: '',
+            errMsg: '必填, 含姓名兩字以上'
+        },
+        
+        birthday: {
+            placeholder: '出生年月日',
+        },
+        
+        gender: {
+            pleaseSelect: '＊請選擇性別',
+            male: '男性',
+            female: '女性',
+        },
+        
+        phone: {
+            placeholder: '聯絡手機',
+            errMsg: '必填, 數字10碼'
+        },
+        
+        address: {
+            placeholder: '聯絡地址',
+        },
+        
+        email: {
+            placeholder: '電子郵件',
+            errMsg: '必填, email格式'
+        },
+        
+        douyin: {
+            placeholder: '個人抖音連結',
+            errMsg: '必填, 完整抖音網址'
+        },
+        
+        facebookid: {
+            placeholder: '個人臉書連結',
+            errMsg: '必填, 完整臉書網址'
+        },
+        
+        performance: {
+            placeholder: '專長 (才藝) 描述',
+        },
+
+        photo: {
+            selected: '已選擇檔案',
+            unselect: '未選擇檔案',
+        },
+
+        submit: {
+            send_text: '送出資料'
+        }
+    },
+
 }
