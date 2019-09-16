6fcc23f0c609ebc9b6090f982bd55176b45aa46d
Wiing <lucusyuan46@gmail.com>
2019-09-16 15:22:12 +0800

    fix download

--- app/Exports/UserExport.php
+++ app/Exports/UserExport.php
@@ -2,7 +2,7 @@
 
 namespace App\Exports;
 
-use App\User;
+use App\Candidate;
 use Maatwebsite\Excel\Concerns\FromCollection;
 
 class UserExport implements FromCollection
@@ -12,6 +12,6 @@ class UserExport implements FromCollection
     */
     public function collection()
     {
-        return User::all();
+        return Candidate::all();
     }
 }
